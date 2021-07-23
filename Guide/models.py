0ace34b64ad14216eee21037acc7b7a12951e9bc
GyeongEun Kim <kekim20@naver.com>
2021-07-24 01:03:43 +0900

    model 생성

--- Guide/models.py
+++ Guide/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Guide (models.Model) :
+    title = models.CharField(max_length=100, blank=False)
+    content = models.TextField(blank=False)
+    writer = models.CharField(max_length=10)
+    date = models.DateField()
+    location = models.CharField(max_length=10)
+    image = models.ImageField(upload_to='')
+    price =models.IntegerField()
+
+class Meta :
+    db_table = Guide
 # Create your models here.
--- Guide/views.py
+++ Guide/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Guide
 
-# Create your views here.
+def show_all_list (request) :
+    lists = Guide.objects.all()
+    return render(request,'guide/list.html',{'guide_lists':lists})
+
+def show_detail(request) :
+    return detail.html
+
+def new_view () :
+    return 
+
+
