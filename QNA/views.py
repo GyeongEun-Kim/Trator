188b8f5827621c3f01c0eb005bb286858fedd032
Pridesd <whwotjr98@naver.com>
2021-07-29 17:16:49 +0900

    내일은 대흉근 하는 날~

--- QNA/views.py
+++ QNA/views.py
@@ -24,6 +24,8 @@ def question_create(request):
     new_question.location = request.POST['location']
     new_question.content = request.POST['content']
     new_question.date = timezone.now()
+    if (request.FILES.get('image') is not None) :
+            new_question.image = request.FILES['image']
     new_question.save()
 
     return redirect('q_detail', new_question.id)
@@ -38,6 +40,7 @@ def question_update(request, id):
     update_question.writer = request.POST['writer']
     update_question.content = request.POST['content']
     update_question.date = timezone.now()
+    update_question.image = request.FILES['image']
     update_question.save()
 
     return redirect('q_detail', update_question.id)
@@ -54,6 +57,8 @@ def answer_new(request, id):
 def answer_create(request, id):
     question = get_object_or_404(Question, pk = id)
     new_answer = Answer(question = question, writer = request.POST['writer'], content = request.POST['content'], date = timezone.now())
+    if (request.FILES.get('image') is not None) :
+            new_answer.image = request.FILES['image']
     new_answer.save()
 
     return redirect('q_detail', id = question.id)
@@ -61,3 +66,23 @@ def answer_create(request, id):
 def answer_detail(request, id):
     answer = get_object_or_404(Answer, pk = id)
     return render(request, 'qna_answer.html', {'answer': answer})
+
+def answer_edit(request, id):
+    edit_answer = Answer.objects.get(id = id)
+    return render(request, 'answer_edit.html', {'answer':edit_answer})
+
+def answer_update(request, id):
+    update_answer = Answer.objects.get(id = id)
+    update_answer.writer = request.POST['writer']
+    update_answer.content = request.POST['content']
+    update_answer.date = timezone.now()
+    update_answer.image = request.FILES['image']
+    update_answer.save()
+
+    return redirect('a_detail', update_answer.id)
+
+def answer_delete(request, id):
+    delete_answer = Answer.objects.get(id = id)
+    id = delete_answer.question.id
+    delete_answer.delete()
+    return redirect('q_detail', id)
