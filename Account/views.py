f75fd9233b58219600dffb922b367e1b99451bac
Jeongwhale <wook971215@gmail.com>
2021-08-01 19:45:42 +0900

    added phone_number, membership and point

--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
+
 from django.contrib import auth
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import redirect, render
@@ -10,6 +13,39 @@ from django.views.generic import CreateView
 
 def index(request):
     return render(request, 'account/index.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(
+                request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+        return redirect("Account:index")
+    else:
+        form = AuthenticationForm()
+        return render(request, "Account/login_view.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("Account:index")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+        return redirect("Account:index")
+    else:
+        form = RegisterForm()
+        return render(request, 'Account/signup_view.html', {"form": form})
 
 
 def signup(request):
