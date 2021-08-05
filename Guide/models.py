af492ce4e3c609d1aa053a7a0a7a459b1a302287
Pridesd <whwotjr98@naver.com>
2021-08-05 23:52:04 +0900

    i hate error

--- Guide/models.py
+++ Guide/models.py
@@ -6,8 +6,8 @@ from Account.models import CustomUser
 class Guide (models.Model) :
     title = models.CharField(max_length=100, blank=False)
     content = models.TextField(blank=False)
-    writer = models.CharField(max_length=10)
-    # writer = models.ForeignKey(CustomUser,on_delete=models.CASCADE, null=True, related_name="author")
+    # writer = models.CharField(max_length=10)
+    writer = models.ForeignKey(CustomUser,on_delete=models.CASCADE, null=True, related_name="author")
     date = models.DateField()
     location = models.CharField(max_length=10)
     image = models.ImageField( blank=True, null=True, upload_to="guide/")
